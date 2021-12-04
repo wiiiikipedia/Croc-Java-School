817ef2f3a65aeb996ba3a7166318cad29a2cb12f
wiiiikipedia <92117258+wiiiikipedia@users.noreply.github.com>
2021-12-04 21:49:25 +0300

    Delete task17-v3 directory

--- task17-v3/src/main/java/org/example/repos/OrdersRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repos;
-
-import org.example.entity.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<Orders, Integer> {
-}
--- task17-v3/src/main/resources/db/migration/V1.0__Create_database_orders.sql
+++
@@ -1,8 +0,0 @@
-create table orders (
-    id serial primary key,
-    order_number int,
-    login text,
-    article text,
-    name text,
-    price int
-);
